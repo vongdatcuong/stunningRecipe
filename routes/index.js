bfdea09b2f99e6cf4ba9de296c5422dcca9b071f
The Anh <nhoxtheanh@gmail.com>
2020-06-05 13:25:34 +0700

    Create PROFILE page

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,32 @@
-var express = require('express');
-var ctrlMain = require('../controllers/main.js');
+var express = require("express");
+var ctrlMain = require("../controllers/main.js");
 
 var router = express.Router();
 
 /* GET Home page. */
-router.get('/', function(req, res) {
-  res.render('dish_detail', {
-		title: 'Stunning Recipe',
-		user: {}
-		//layout: false
-	});
+router.get("/", function (req, res) {
+  //   res.render('yourProfile', {
+  // 		title: 'Stunning Recipe',
+  // 		user: {}
+  // 		//layout: false
+  // 	});
+  res.render("profile", {
+    title: "Stunning Recipe",
+    user: {},
+    //layout: false
+  });
 });
 
-router.get('/contact', ctrlMain.contact);
+router.get("/contact", ctrlMain.contact);
 
-router.get('/about', ctrlMain.about);
+router.get("/about", ctrlMain.about);
 
-router.get('/privacy', ctrlMain.privacy);
+router.get("/privacy", ctrlMain.privacy);
 
-router.get('/terms', ctrlMain.terms);
+router.get("/terms", ctrlMain.terms);
 
-router.get('/help', ctrlMain.help);
+router.get("/help", ctrlMain.help);
 
-router.get('/faqs', ctrlMain.faqs);
+router.get("/faqs", ctrlMain.faqs);
 
 module.exports = router;
